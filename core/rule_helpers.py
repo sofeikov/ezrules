ddcd77f62fb1e120c44e3817448c34d255cf4e2d
Konstantin Sofeikov <sofeykov@gmail.com>
2023-10-29 20:20:24 +0000

    Dollar sign replacer

--- core/rule_helpers.py
+++ core/rule_helpers.py
@@ -1,4 +1,5 @@
 import ast
+import pyparsing as pp
 from typing import Any
 
 
@@ -12,3 +13,21 @@ class RuleParamExtractor(ast.NodeVisitor):
             if node.value.id == "t" and isinstance(node.slice, ast.Constant):
                 self.params.add(node.slice.value)
         return node
+
+
+class DollarNotationConverter:
+    @staticmethod
+    def replace_with_matched_text(tokens):
+        return f'f["{tokens[0][1:]}"]'
+
+    def __init__(self):
+        dollar_word = pp.Combine("$" + pp.Word(pp.alphas + "_", pp.alphanums + "_"))
+
+        line_parser = dollar_word
+        line_parser.ignore(pp.QuotedString('"'))
+        # line_parser.ignore(pp.QuotedString("'"))
+        line_parser.setParseAction(self.replace_with_matched_text)
+        self._parser = line_parser
+
+    def replace_dollar_notation(self, code: str):
+        return self._parser.transform_string(code)
---
+++ tests/core/test_rule_helpers.py
@@ -0,0 +1,55 @@
+import pytest
+from core.rule_helpers import DollarNotationConverter
+
+
+@pytest.mark.parametrize(
+    "input_code, expected_output",
+    [
+        (
+            '"This is $text within quotes."',
+            '"This is $text within quotes."',
+        ),  # Dollar notation within quoted string
+        (
+            '$variable inside "$quoted string".',
+            'f["variable"] inside "$quoted string".',
+        ),  # Dollar notation and quoted string
+        (
+            '"No $variable inside quotes."',
+            '"No $variable inside quotes."',
+        ),  # No dollar notation within quoted string
+        (
+            '$1invalid inside "$quoted $string".',
+            '$1invalid inside "$quoted $string".',
+        ),  # Invalid variable inside quoted string
+        (
+            '$@ inside "$quoted $string".',
+            '$@ inside "$quoted $string".',
+        ),  # Invalid variable inside quoted string
+    ],
+)
+def test_replace_dollar_notation_quotes(input_code, expected_output):
+    converter = DollarNotationConverter()
+    result = converter.replace_dollar_notation(input_code)
+    assert result == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_code, expected_output",
+    [
+        ("$variable", 'f["variable"]'),
+        (
+            "This is $text with $multiple $variables.",
+            'This is f["text"] with f["multiple"] f["variables"].',
+        ),
+        (
+            "$1invalid",
+            "$1invalid",
+        ),  # Dollar notation should not be applied to invalid variable names
+        ("$@", "$@"),  # Dollar notation should not be applied to invalid variable names
+        ("No variables here.", "No variables here."),  # No dollar notation in the input
+    ],
+)
+def test_replace_dollar_notation(input_code, expected_output):
+    converter = DollarNotationConverter()
+    result = converter.replace_dollar_notation(input_code)
+    assert result == expected_output
