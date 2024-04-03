3d39ac58cf18cf1382d55514ff454443065091c4
Konstantin Sofeikov <sofeykov@gmail.com>
2024-04-03 19:27:49 +0100

    Works in EKS

--- scripts/build_and_push_manager.sh
+++ scripts/build_and_push_manager.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 349229516285.dkr.ecr.eu-west-1.amazonaws.com
 
-docker build -t ezrule-manager:latest -f Dockerfile.manager .
+docker buildx build --platform linux/amd64 -t ezrule-manager:latest -f Dockerfile.manager .
 
 docker tag ezrule-manager:latest 349229516285.dkr.ecr.eu-west-1.amazonaws.com/ezrule-manager:latest
 
